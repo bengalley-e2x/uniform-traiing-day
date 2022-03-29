1c01140156efa3c02930b5bee6e1ce3caaa013f6
ben.galley <benjamin.galley@e2x.com>
2022-03-29 11:46:45 +0100

    add composition from uniform passing into props

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,42 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { CanvasClient } from "@uniformdev/canvas";
+import { Composition } from "@uniformdev/canvas-react";
 
-export default function Home() {
+export async function getStaticProps() {
+  const client = new CanvasClient({
+    apiKey: process.env.UNIFORM_API_KEY,
+    projectId: process.env.UNIFORM_PROJECT_ID,
+  });
+  const { composition } = await client.getCompositionBySlug({
+    slug: "/",
+  });
+  return {
+    props: {
+      composition,
+    },
+  };
+}
+
+export default function Home({ composition }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Uniform Enablement</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Composition data={composition}>
+      <div className={styles.container}>
+        <Head>
+          <title>{composition.parameters.title?.value}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>[header]</h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>[header]</h1>
 
-        <p className={styles.description}>[body]</p>
-      </main>
+          <p className={styles.description}>[body]</p>
+        </main>
 
-      <footer className={styles.footer}>
-        <div>[footer]</div>
-      </footer>
-    </div>
+        <footer className={styles.footer}>
+          <div>[footer]</div>
+        </footer>
+      </div>
+    </Composition>
   );
 }
