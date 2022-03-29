83c0e29ff9c03f706d05241177b020a05750e759
ben.galley <benjamin.galley@e2x.com>
2022-03-29 12:43:10 +0100

    add a full page

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 export const Header = ({ component }) => {
   return <h1 className={styles.title}>Welcome!</h1>;
